d68eb5739d908153b039971b704334883d769a16
Bastien <104871981+300n@users.noreply.github.com>
2023-10-06 14:24:11 +0200

    Update Get_stat.h

--- Get_stat.h
+++ Get_stat.h
@@ -19,7 +19,7 @@ int get_number_of_particle()
     int out = 0;
     for (int i = 0; i<matlength; i++) {
         for (int j = 0; j<matwidth; j++ ) {
-            if (mat.data[i][j].x>=(width/30)-10 && mat.data[i][j].x<=width-(width/30)+10 && mat.data[i][j].y>=(height/30)-10 && mat.data[i][j].y<=height-(height/30)+10) {
+            if (particle_grid.data[i][j].position.x>=(width/30)-10 && particle_grid.data[i][j].position.x<=width-(width/30)+10 && particle_grid.data[i][j].position.y>=(height/30)-10 && particle_grid.data[i][j].position.y<=height-(height/30)+10) {
                 out++;
             }
         }
